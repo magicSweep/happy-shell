def38f906895fbf406367ef1eace661554ac207a
magicLost <oooze@mail.ru>
2021-07-26 14:29:02 +0300

    Ready to publish

--- .np-config.js
+++ .np-config.js
@@ -1,9 +1,14 @@
 module.exports = {
   branch: "main",
+  // Run npm test
   tests: false,
+  // Cleanup node_modules
   cleanup: false,
   yarn: false,
+  // Subdirectory to publish
   contents: "src",
+  // Open a GitHub release draft after releasing
   releaseDraft: false,
-  preview: true,
+  // Show tasks without actually executing them
+  preview: false,
 };
